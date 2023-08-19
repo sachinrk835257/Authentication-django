5166505df02d1e9fd0482c5b051000a90c1e3680
sachin sen <sachinrk835257@gmail.com>
2023-08-19 19:14:54 +0530

    done

--- core/authapp/views.py
+++ core/authapp/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def login_page(request):
-    return HttpResponse("login")
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(email,password)
+
+        return redirect('/auth/login/')
+
+
+    return render(request,'auth/login.html')
 
 def signup(request):
-    return HttpResponse("signup")
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        print(email,pass1,pass2)
+        if pass1 != pass2 :
+            return redirect('/auth/signup/')
+    
+        return redirect('/auth/signup/')
+
+
+    return render(request,'auth/signup.html')
 
 def logout_page(request):
     return HttpResponse("logout")
